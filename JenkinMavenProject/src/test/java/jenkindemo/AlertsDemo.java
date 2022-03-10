f615c0eaafb0c497455c0ff90b25ea2997a80c26
shiva <shiva@ShivaReddy>
2022-03-10 18:06:30 +0530

    Ademo

---
+++ JenkinMavenProject/src/test/java/jenkindemo/AlertsDemo.java
@@ -0,0 +1,29 @@
+package jenkindemo;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AlertsDemo {
+	public static void main(String[] args) throws Exception
+	{
+	System.setProperty("webdriver.chrome.driver","C:\\Testing softwares\\chromedriver_win32\\chromedriver.exe");
+
+	WebDriver driver= new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("http://demo.automationtesting.in/Alerts.html");
+	driver.findElement(By.xpath("//button[@class='btn btn-danger']")).click();
+
+	Alert alt= driver.switchTo().alert();
+	String altmsg= driver.switchTo().alert().getText();
+	System.out.println("Alert msg is:" +altmsg);
+	Thread.sleep(3000);
+	alt.accept();
+	driver.close();
+	}
+	}
+
+
+
+
